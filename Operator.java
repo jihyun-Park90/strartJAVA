b8a34d0b0ffc8ca8d25d311b7ba9f50d9344a687
jihyun-Park90 <76693383+jihyun-Park90@users.noreply.github.com>
2021-03-29 18:00:48 +0900

    operator
    기본 연산자

---
+++ Operator.java
@@ -0,0 +1,16 @@
+package Operator;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		System.out.println(6+6); //12
+		System.out.println(6-6); //0
+		System.out.println(6*6); //36
+		System.out.println(6/6); //1
+		
+		System.out.println(Math.PI); //3.141592...
+		System.out.println(Math.floor(Math.PI)); //내림
+		System.out.println(Math.ceil(Math.PI)); //올림
+	}
+
+}
